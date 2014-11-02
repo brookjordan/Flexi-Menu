b8077f0ccebe94cd1a4b532fef14dfd5aadb415c
brkjrdn <bjmonkey@gmail.com>
2014-11-02 20:04:17 +0000

    Added temp folder
    Added the grunt copy module and set up.
    
    Created a js_ready folder in src for files which are production ready
    and need only be copied.
    
    Copying the minified angular file to /js on initial grunt.
    
    Moved ‘created’, but not ‘final’, files to a temp folder.

--- Gruntfile.js
+++ Gruntfile.js
@@ -8,6 +8,24 @@ grunt.initConfig({
 
 
 
+  ngdocs: {
+    all: [ 'src/js/**/*.js', ],
+  },
+
+
+
+  copy: {
+    angular: {
+      expand: true,
+      flatten: true,
+      cwd: 'src/js_ready/',
+      src: '**',
+      dest: 'js/',
+    },
+  },
+
+
+
   sass: {
     dist: {
       options: {
@@ -17,7 +35,7 @@ grunt.initConfig({
         sourcemap: 'inline',
       },
       files: {
-        'src/css_standards/base.css': 'src/scss/base.scss',
+        'temp/css/standards/base.css': 'src/scss/base.scss',
       },
     },
   },
@@ -27,30 +45,20 @@ grunt.initConfig({
   autoprefixer: {
 
     options: {
+      browsers: [ 'last 10 versions', 'ie 8', 'ie 9', ],
+      map: true,
+
       cascade: debug_mode ?
         true :
         false,
-      browsers: [ 'last 10 versions', 'ie 8', 'ie 9', ],
-
-      map: {
-        inline: true,
-        sourceContent: true,
-        annotation: true,
-      },
     },
 
     single_file: {
       options: {},
 
-      src: 'src/css_standards/base.css',
+      src: 'temp/css/standards/base.css',
       dest: 'css/base.css',
     },
-  },
-
-
-
-  ngdocs: {
-    all: [ 'src/js/**/*.js', ],
   },
 
 
@@ -82,7 +90,7 @@ grunt.initConfig({
 
     myApp:        {
       src:      'templates/**/**.html',
-      dest:     'src/js/my_app/templates.js',
+      dest:     'temp/js/my_app/templates.js',
     },
   },
 
@@ -95,12 +103,12 @@ grunt.initConfig({
 
     mu_system: {
       src: [ 'src/js/MU_system/app.js', 'src/js/MU_system/services/muMain.js', 'src/js/MU_system/services/muContent.js', 'src/js/MU_system/directives/*.js', ],
-      dest: 'src/js_concat/MU_system.js',
+      dest: 'temp/js/concat/MU_system.js',
     },
 
     my_app: {
-      src: [ 'src/js/my_app/app.js', 'src/js/my_app/templates.js', 'src/js/my_app/services/*.js', 'src/js/my_app/directives/*.js', ],
-      dest: 'src/js_concat/my_app.js',
+      src: [ 'src/js/my_app/app.js', 'temp/js/my_app/templates.js', 'src/js/my_app/services/*.js', 'src/js/my_app/directives/*.js', ],
+      dest: 'temp/js/concat/my_app.js',
     },
   },
 
@@ -121,7 +129,7 @@ grunt.initConfig({
         beautify: debug_mode ? true : false,
       },
       files: {
-        'js/all.min.js': [ 'src/js_concat/MU_system.js', 'src/js_concat/my_app.js', ],
+        'js/all.min.js': [ 'temp/js/concat/MU_system.js', 'temp/js/concat/my_app.js', ],
       },
     },
   },
@@ -178,20 +186,21 @@ grunt.initConfig({
 
 
 
+grunt.loadNpmTasks('grunt-ngdocs');
+
+grunt.loadNpmTasks('grunt-contrib-copy');
 
 grunt.loadNpmTasks('grunt-contrib-sass');
 grunt.loadNpmTasks('grunt-autoprefixer');
 
+grunt.loadNpmTasks('grunt-angular-templates');
 grunt.loadNpmTasks('grunt-contrib-concat');
 grunt.loadNpmTasks('grunt-contrib-uglify');
-grunt.loadNpmTasks('grunt-angular-templates');
-
-grunt.loadNpmTasks('grunt-ngdocs');
 
 grunt.loadNpmTasks('grunt-contrib-watch');
 
 // Default tasks.
-grunt.registerTask('default', ['sass', 'autoprefixer', 'ngtemplates', 'concat', 'uglify', 'watch', ]);
+grunt.registerTask('default', ['copy', 'sass', 'autoprefixer', 'ngtemplates', 'concat', 'uglify', 'watch', ]);
 grunt.registerTask('ngdocs', ['ngdocs', ]);
 
 };
---
+++ temp/js/my_app/templates.js
@@ -0,0 +1,48 @@
+angular.module('myApp').run(['$templateCache', function($templateCache) {
+  'use strict';
+
+  $templateCache.put('templates/content/center.html',
+    "<div hero-area></div><p class=widthDisplay></p><div game-grid></div><div test-buttons></div>"
+  );
+
+
+  $templateCache.put('templates/directives/gameGridDirective.html',
+    "<div class=gameGrid><div class=gameGrid__game ng-repeat=\"game in gameGrid.games track by game.id\"><div class=gameGrid__game__inner ng-style=\"{ 'background-color': game.bg }\"></div></div></div>"
+  );
+
+
+  $templateCache.put('templates/directives/heroAreaDirective.html',
+    "<div class=hero ng-style=hero.contentHeight()><h1 class=hero__title>{{ hero.title }}</h1></div>"
+  );
+
+
+  $templateCache.put('templates/directives/leftMenuDirective.html',
+    "<div class=\"MU__menu-left MU__menu-left--{{ !MU_left.menus.isVisible( 'left' ) && 'hidden' || MU_left.menus.state( 'left' ) }}\" ng-style=MU_left.leftStyle()><div class=MU__menu-left__inner><div class=\"MU__navIcons MU__navIcons-left\"><button class=MU__navIcons__item ng-click=\"MU_left.toggle( $index )\" ng-repeat=\"button in MU_left.items\" ng-class=\"{ 'MU__navIcons-left__item--active': MU_left.openItem === $index }\">{{ $index }}</button></div><div class=MU-you><p class=MU__mainNavItem>ONE</p><p class=MU__mainNavItem>ONE</p><p class=MU__mainNavItem>ONE</p><p class=MU__mainNavItem>ONE</p><p class=MU__mainNavItem>ONE</p><p class=MU__mainNavItem>ONE</p><p class=MU__mainNavItem>ONE</p></div></div></div>"
+  );
+
+
+  $templateCache.put('templates/directives/muSystemDirective.html',
+    "<div class=\"MU {{ MU.content.mediaClass }} {{ MU.getClassName() }}\" id=MU ng-style=MU.systemStyle()><div class=\"MU__menu-top MU__menu-top--{{ !MU.menus.isVisible( 'top' ) && 'hidden' || MU.menus.state( 'top' ) }}\" ng-style=MU.topStyle()><div class=MU__menu-top__inner ng-include=\" 'templates/menus/top.html' \"></div></div><div left-menu></div><div right-menu></div><div class=\"MU__menu-bottom MU__menu-bottom--{{ !MU.menus.isVisible( 'bottom' ) && 'hidden' || MU.menus.state( 'bottom' ) }}\" ng-style=MU.bottomStyle()><div class=MU__menu-bottom__inner ng-include=\" 'templates/menus/bottom.html' \"></div></div><div class=MU__center><div class=MU__center__scroll><div class=MU__center__content ng-include=\" 'templates/content/center.html' \"></div></div></div></div>"
+  );
+
+
+  $templateCache.put('templates/directives/rightMenuDirective.html',
+    "<div class=\"MU__menu-right MU__menu-right--{{ !MU_right.menus.isVisible( 'right' ) && 'hidden' || MU_right.menus.state( 'right' ) }}\" ng-style=MU_right.rightStyle()><div class=MU__menu-right__inner><div class=\"MU__navIcons MU__navIcons-right\"><button class=MU__navIcons__item ng-click=\"MU_right.toggle( $index )\" ng-repeat=\"button in MU_right.items\" ng-class=\"{ 'MU__navIcons-right__item--active': MU_right.openItem === $index }\">{{ $index }}</button></div><div class=MU-me><p class=MU__mainNavItem>ONE</p><p class=MU__mainNavItem>ONE</p><p class=MU__mainNavItem>ONE</p><p class=MU__mainNavItem>ONE</p></div></div></div>"
+  );
+
+
+  $templateCache.put('templates/directives/testButtonsDirective.html',
+    "<div class=testButtons><hr style=\"clear: both\"><span>Toggle open:</span><br><button ng-repeat=\"(title, menu) in testButtons.menus.menus track by title\" ng-click=\"testButtons.menus.toggleState( title )\">{{ title }}</button><hr><span>Toggle visiblity:</span><br><button ng-repeat=\"(title, menu) in testButtons.menus.menus track by title\" ng-click=\"testButtons.menus.toggleVisibility( title )\"><span ng-if=\"testButtons.menus.isVisible( title )\">Hide {{ title }}</span> <span ng-if=\"!testButtons.menus.isVisible( title )\">Show {{ title }}</span></button><hr><span>Prioritise:</span><br><button ng-repeat=\"(title, menu) in testButtons.menus.menus track by title\" ng-click=\"testButtons.menus.reorder( title )\">Lift {{ title }}</button><hr><span>Link:</span> <button ng-show=\"testButtons.menus.menus['left'] && testButtons.menus.menus['right']\" ng-click=\"testButtons.linkMenus( 'left right' )\">left and right</button> <button ng-show=\"testButtons.menus.menus['top'] && testButtons.menus.menus['bottom']\" ng-click=\"testButtons.linkMenus( 'top bottom' )\">top and bottom</button><br><span>Unlink:</span> <button ng-show=\"testButtons.menus.menus['left'] && testButtons.menus.menus['right']\" ng-click=\"testButtons.unlinkMenus( 'left right' )\">left and right</button> <button ng-show=\"testButtons.menus.menus['top'] && testButtons.menus.menus['bottom']\" ng-click=\"testButtons.unlinkMenus( 'top bottom' )\">top and bottom</button> <button ng-click=testButtons.menus.unlink()>all</button></div>"
+  );
+
+
+  $templateCache.put('templates/menus/bottom.html',
+    "- -- --- -- - Cookies - -- --- -- - <button class=MU__menu-bottom__switch ng-click=\"MU.menus.toggleVisibility( 'bottom' )\"></button>"
+  );
+
+
+  $templateCache.put('templates/menus/top.html',
+    "<nav class=MU__menu-top__nav>ONE - - TWO - - THREE - - FOUR</nav><div class=MU__menu-top__switch><button class=MU__menu-top__switch__right ng-click=\"MU.menus.toggleState( 'top' )\">Toggle Nav</button></div>"
+  );
+
+}]);
